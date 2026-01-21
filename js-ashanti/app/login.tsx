9f3f4a090b102645d2ca83c259f943100d4366cb
michaelperryjnr <michaelperryt97@gmail.com>
2026-01-21 19:14:42 +0000

    fixed mobile login

--- js-ashanti/app/login.tsx
+++ js-ashanti/app/login.tsx
@@ -52,6 +52,7 @@ export default function LoginScreen() {
 
     try {
       // Call better-auth API endpoint
+      console.log("tried to log tf in")
       const response = await apiRequest(API_ENDPOINTS.AUTH.LOGIN, {
         method: "POST",
         body: JSON.stringify({
@@ -59,6 +60,8 @@ export default function LoginScreen() {
           password,
         }),
       });
+
+      console.log("response", response) 
 
       if (response.error) {
         setError(response.error.message || "Login failed");
